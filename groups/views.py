7cfb8e6154b796b80e2b1383ababac3f457f0b37
Sourabh <msourabh970320@gmail.com>
2021-03-15 22:40:34 +0530

    all_files_again_2

--- groups/views.py
+++ groups/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import (LoginRequiredMixin,
 from django.urls import reverse
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from groups.models import Group, GroupMember, Teachers
+from groups.models import Group, GroupMember
 # Create your views here.
 
 
@@ -21,11 +21,9 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
-    teachers = Teachers
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['teachers'] = self.teachers
         return context
 
 
